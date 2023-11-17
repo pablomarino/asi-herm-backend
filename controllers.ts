8e7eaee67bd4c6227cc5688c4cb241fb5ab9e923
Xaime Pardal <xaime_pardal@outlook.com>
2023-11-17 14:10:14 +0100

    orders and boxes API / add Postman collection to test

--- controllers.ts
+++ controllers.ts
@@ -11,13 +11,14 @@ interface Item {
 
 interface Box {
   _id: { $oid: string };
+  reference: string;
   size: string;
   location: string;
-  itemId: { $oid: string }; // Referencia a Item
+  itemReference: string; // Referencia a Item
 }
 
 interface OrderItem {
-  itemId: { $oid: string }; // Referencia a Item
+  itemReference: string; // Referencia a Item
   numberItems: number; // Cantidad de items
 }
 
@@ -212,7 +213,418 @@ const deleteItem = async ({
   }
 };
 
-export { getItems, getItem, addItem, updateItem, deleteItem };
+const boxes = db.collection<Box>("boxes");
 
-// const boxes = db.collection<Box>("boxes");
-// const orders = db.collection<Order>("orders");
+// DESC: GET all Boxes
+// METHOD GET /api/boxes
+const getBoxes = async ({ response }: { response: any }) => {
+  try {
+    // Find all boxes and convert them into an Array
+    const allBoxes = await boxes.find({}).toArray();
+    console.log(allBoxes);
+    if (allBoxes) {
+      response.status = 200;
+      response.body = {
+        success: true,
+        data: allBoxes,
+      };
+    } else {
+      response.status = 500;
+      response.body = {
+        success: false,
+        msg: "Internal Server Error",
+      };
+    }
+  } catch (err) {
+    response.body = {
+      success: false,
+      msg: err.toString(),
+    };
+  }
+};
+
+// DESC: GET single box
+// METHOD: GET /api/boxes/:id
+const getBox = async ({
+  params,
+  response,
+}: {
+  params: { id: string };
+  response: any;
+}) => {
+  // Searches for a particular box in the DB
+  const box = await boxes.findOne({ reference: params.id });
+  // If found, respond with the box. If not, respond with a 404
+  if (box) {
+    response.status = 200;
+    response.body = {
+      success: true,
+      data: box,
+    };
+  } else {
+    response.status = 404;
+    response.body = {
+      success: false,
+      msg: "No box found",
+    };
+  }
+};
+
+// DESC: ADD single box
+// METHOD: POST /api/boxes
+const addBox = async ({
+  request,
+  response,
+}: {
+  request: any;
+  response: any;
+}) => {
+  try {
+    // If the request has no Body, it will return a 400
+    if (!request.hasBody) {
+      response.status = 400;
+      response.body = {
+        success: false,
+        msg: "No Data",
+      };
+    } else {
+      // Otherwise, it will try to insert
+      // a box in the DB and respond with 201
+      const body = await request.body();
+      const box: Box = await body.value;
+
+      const exists = await boxes.findOne({ reference: box.reference });
+      const existItem = await items.findOne({ reference: box.itemReference });
+      if (exists) {
+        response.status = 400;
+        response.body = {
+          success: false,
+          msg: "Box with the same reference already exists",
+        };
+      } else if (!existItem) {
+        response.status = 400;
+        response.body = {
+          success: false,
+          msg: "Referenced item not exists",
+        };
+      } else {
+        await boxes.insertOne(box);
+        response.status = 201;
+        response.body = {
+          success: true,
+          data: box,
+        };
+      }
+    }
+  } catch (err) {
+    response.body = {
+      success: false,
+      msg: err.toString(),
+    };
+  }
+};
+
+// DESC: UPDATE single box
+// METHOD: PUT /api/boxes/:id
+const updateBox = async ({
+  params,
+  request,
+  response,
+}: {
+  params: { id: string };
+  request: any;
+  response: any;
+}) => {
+  try {
+    // Search a box in the DB and update with given values if found
+    const body = await request.body();
+    const inputBox: Box = await body.value;
+
+    const existItem = await items.findOne({
+      reference: inputBox.itemReference,
+    });
+    if (!existItem) {
+      response.status = 400;
+      response.body = {
+        success: false,
+        msg: "Referenced item not exists",
+      };
+    } else {
+      await boxes.updateOne(
+        { reference: params.id },
+        {
+          $set: {
+            size: inputBox.size,
+            location: inputBox.location,
+            itemReference: inputBox.itemReference,
+          },
+        }
+      );
+      // Respond with the Updated Box
+      const updatedBox = await boxes.findOne({ reference: params.id });
+      response.status = 200;
+      response.body = {
+        success: true,
+        data: updatedBox,
+      };
+    }
+  } catch (err) {
+    response.body = {
+      success: false,
+      msg: err.toString(),
+    };
+  }
+};
+
+// DESC: DELETE single box
+// METHOD: DELETE /api/boxes/:id
+const deleteBox = async ({
+  params,
+  response,
+}: {
+  params: { id: string };
+  request: any;
+  response: any;
+}) => {
+  try {
+    // Search for the given box and drop it from the DB
+    await boxes.deleteOne({ reference: params.id });
+    response.status = 201;
+    response.body = {
+      success: true,
+      msg: "Box deleted",
+    };
+  } catch (err) {
+    response.body = {
+      success: false,
+      msg: err.toString(),
+    };
+  }
+};
+
+const orders = db.collection<Order>("orders");
+
+// DESC: GET all Orders
+// METHOD GET /api/orders
+const getOrders = async ({ response }: { response: any }) => {
+  try {
+    // Find all orders and convert them into an Array
+    const allOrders = await orders.find({}).toArray();
+    console.log(allOrders);
+    if (allOrders) {
+      response.status = 200;
+      response.body = {
+        success: true,
+        data: allOrders,
+      };
+    } else {
+      response.status = 500;
+      response.body = {
+        success: false,
+        msg: "Internal Server Error",
+      };
+    }
+  } catch (err) {
+    response.body = {
+      success: false,
+      msg: err.toString(),
+    };
+  }
+};
+
+// DESC: GET single order
+// METHOD: GET /api/orders/:id
+const getOrder = async ({
+  params,
+  response,
+}: {
+  params: { id: string };
+  response: any;
+}) => {
+  // Searches for a particular order in the DB
+  const order = await orders.findOne({ reference: params.id });
+  // If found, respond with the order. If not, respond with a 404
+  if (order) {
+    response.status = 200;
+    response.body = {
+      success: true,
+      data: order,
+    };
+  } else {
+    response.status = 404;
+    response.body = {
+      success: false,
+      msg: "No order found",
+    };
+  }
+};
+
+// DESC: ADD single order
+// METHOD: POST /api/orders
+const addOrder = async ({
+  request,
+  response,
+}: {
+  request: any;
+  response: any;
+}) => {
+  try {
+    // If the request has no Body, it will return a 400
+    if (!request.hasBody) {
+      response.status = 400;
+      response.body = {
+        success: false,
+        msg: "No Data",
+      };
+    } else {
+      // Otherwise, it will try to insert
+      // a order in the DB and respond with 201
+      const body = await request.body();
+      const order = await body.value;
+
+      const exists = await orders.findOne({ reference: order.reference });
+      if (exists) {
+        response.status = 400;
+        response.body = {
+          success: false,
+          msg: "Order with the same reference already exists",
+        };
+      } else {
+        const orderItems: OrderItem[] = order.items;
+        let allItemsExist = true;
+        for (const item of orderItems) {
+          const existItem = await items.findOne({
+            reference: item.itemReference,
+          });
+          if (!existItem) {
+            allItemsExist = false;
+            break;
+          }
+        }
+
+        if (allItemsExist) {
+          await orders.insertOne(order);
+          response.status = 201;
+          response.body = {
+            success: true,
+            data: order,
+          };
+        } else {
+          response.status = 400;
+          response.body = {
+            success: false,
+            msg: "There are items that not exists in BD in your order",
+          };
+        }
+      }
+    }
+  } catch (err) {
+    response.body = {
+      success: false,
+      msg: err.toString(),
+    };
+  }
+};
+
+// DESC: UPDATE single order
+// METHOD: PUT /api/order/:id
+const updateOrder = async ({
+  params,
+  request,
+  response,
+}: {
+  params: { id: string };
+  request: any;
+  response: any;
+}) => {
+  try {
+    // Search a order in the DB and update with given values if found
+    const body = await request.body();
+    const inputOrder: Order = await body.value;
+
+    let allItemsExist = true;
+    for (const item of inputOrder.items) {
+      const existItem = await items.findOne({
+        reference: item.itemReference,
+      });
+      if (!existItem) {
+        allItemsExist = false;
+        break;
+      }
+    }
+    if (allItemsExist) {
+      await orders.updateOne(
+        { reference: params.id },
+        {
+          $set: {
+            date: inputOrder.date,
+            state: inputOrder.state,
+            items: inputOrder.items,
+          },
+        }
+      );
+      // Respond with the Updated Order
+      const updatedOrder = await orders.findOne({ reference: params.id });
+      response.status = 200;
+      response.body = {
+        success: true,
+        data: updatedOrder,
+      };
+    } else {
+      response.status = 400;
+      response.body = {
+        success: false,
+        msg: "There are items that not exists in BD in your order",
+      };
+    }
+  } catch (err) {
+    response.body = {
+      success: false,
+      msg: err.toString(),
+    };
+  }
+};
+
+// DESC: DELETE single order
+// METHOD: DELETE /api/orders/:id
+const deleteOrder = async ({
+  params,
+  response,
+}: {
+  params: { id: string };
+  request: any;
+  response: any;
+}) => {
+  try {
+    // Search for the given order and drop it from the DB
+    await orders.deleteOne({ reference: params.id });
+    response.status = 201;
+    response.body = {
+      success: true,
+      msg: "Order deleted",
+    };
+  } catch (err) {
+    response.body = {
+      success: false,
+      msg: err.toString(),
+    };
+  }
+};
+
+export {
+  getItems,
+  getItem,
+  addItem,
+  updateItem,
+  deleteItem,
+  getBoxes,
+  getBox,
+  addBox,
+  updateBox,
+  deleteBox,
+  getOrders,
+  getOrder,
+  addOrder,
+  updateOrder,
+  deleteOrder,
+};
--- routes.ts
+++ routes.ts
@@ -5,39 +5,37 @@ import {
   addItem,
   updateItem,
   deleteItem,
-  
-  // getBoxes,
-  // getBox,
-  // addBox,
-  // updateBox,
-  // deleteBox,
-
-  // getOrders,
-  // getOrder,
-  // addOrder,
-  // updateOrder,
-  // deleteOrder
+  getBoxes,
+  getBox,
+  addBox,
+  updateBox,
+  deleteBox,
+  getOrders,
+  getOrder,
+  addOrder,
+  updateOrder,
+  deleteOrder,
 } from "./controllers.ts";
 
 const router = new Router();
 
 router
-  .get("/api/items", getItems) 
-  .get("/api/items/:id", getItem) 
-  .post("/api/items", addItem) 
-  .put("/api/items/:id", updateItem) 
-  .delete("/api/items/:id", deleteItem) 
+  .get("/api/items", getItems)
+  .get("/api/items/:id", getItem)
+  .post("/api/items", addItem)
+  .put("/api/items/:id", updateItem)
+  .delete("/api/items/:id", deleteItem)
 
-  // .get("/api/boxes", getBoxes) 
-  // .get("/api/boxes/:id", getBox) 
-  // .post("/api/boxes", addBox) 
-  // .put("/api/boxes", updateBox) 
-  // .delete("/api/boxes", deleteBox)
+  .get("/api/boxes", getBoxes)
+  .get("/api/boxes/:id", getBox)
+  .post("/api/boxes", addBox)
+  .put("/api/boxes/:id", updateBox)
+  .delete("/api/boxes/:id", deleteBox)
 
-  // .get("/api/orders", getOrders) 
-  // .get("/api/orders/:id", getOrder) 
-  // .post("/api/orders", addOrder) 
-  // .put("/api/orders", updateOrder) 
-  // .delete("/api/orders", deleteOrder)
+  .get("/api/orders", getOrders)
+  .get("/api/orders/:id", getOrder)
+  .post("/api/orders", addOrder)
+  .put("/api/orders/:id", updateOrder)
+  .delete("/api/orders/:id", deleteOrder);
 
 export default router;
